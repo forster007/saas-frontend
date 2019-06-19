20ea134cd0fc2b5848783d2281117c44b3d3721a
forster007 <forster007@gmail.com>
2019-06-19 01:55:55 -0300

    Frontend stuffs

---
+++ build/precache-manifest.8d6f97efd9852766a5f9d81f0e360852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a4c1d13de66016711ba48064e97cdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ffcaca4930351bd05e",
+    "url": "/static/css/2.19b13f22.chunk.css"
+  },
+  {
+    "revision": "90ffcaca4930351bd05e",
+    "url": "/static/js/2.491f42ed.chunk.js"
+  },
+  {
+    "revision": "488adda96b8de27c6e75",
+    "url": "/static/js/main.8c6478a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
